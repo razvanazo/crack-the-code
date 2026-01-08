bd8921dba20383aff311fbbe55a1a47696a370ac
Razvan Azoiei <razvan@Razvans-MacBook-Pro.local>
2026-01-09 01:09:55 +0200

    add chat to game room

--- frontend/src/views/testPage.vue
+++ frontend/src/views/testPage.vue
@@ -1,20 +1,9 @@
 <script setup>
-
-import PlayerIconTimer from "@/components/PlayerIconTimer.vue";
-import {ref} from "vue";
-
-const opponentRef = ref(null);
-const playerRef = ref(null);
-
-function done() {
-    opponentRef.value.start();
-}
+import ChatPopup from "@/components/ChatPopup.vue";
 </script>
 
 <template>
-    <PlayerIconTimer ref="opponentRef" myClass="opponent"/>
-    <PlayerIconTimer ref="playerRef" myClass="player"/>
-    <button @click="done"></button>
+    <ChatPopup/>
 </template>
 
 <style scoped>
